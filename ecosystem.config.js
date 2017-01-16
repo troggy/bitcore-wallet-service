646b9c7ad43d46e7a82988eb6684c8113f82aa15
Kosta Korenkov <kosta@whoot.me>
2017-01-16 19:06:23 +0300

    feat: setup for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps : [
+    {
+      name      : "locker",
+      script    : "locker/locker.js"
+    },
+    {
+      name      : "messagebroker",
+      script    : "messagebroker/messagebroker.js"
+    },
+    {
+      name      : "bcmonitor",
+      script    : "bcmonitor/bcmonitor.js"
+    },
+/*    {
+      name      : "emailservice",
+      script    : "emailservice/emailservice.js"
+    },*/
+    {
+      name      : "pushnotificationsservice",
+      script    : "pushnotificationsservice/pushnotificationsservice.js"
+    },
+    {
+      name      : "fiatrateservice",
+      script    : "fiatrateservice/fiatrateservice.js"
+    },
+    {
+      name      : "bws",
+      script    : "bws.js"
+    }
+  ]
+};
